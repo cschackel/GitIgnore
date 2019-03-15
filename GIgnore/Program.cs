4ac8305535eefc4bce433e1a93c71054bec8b9a7
Cirus Sckackel <cschackel@my.wctc.edu>
2019-03-15 08:06:47 -0500

    Added New Message

--- GIgnore/Program.cs
+++ GIgnore/Program.cs
@@ -7,7 +7,7 @@ namespace GIgnore
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            Console.WriteLine("Goodbye World!");
         }
     }
 }
